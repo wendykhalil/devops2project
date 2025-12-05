788d48c117bc3dc368325bde9f307643f3cceffd
khalil wendy <159011515+wendykhalil@users.noreply.github.com>
2025-12-05 19:33:19 +0100

    Initialize Spring Boot application in App.java
    Updated main method to run Spring Boot application.

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
+        SpringApplication.run(App.class, args);
 
-        System.out.println("Hello World!");
+        // Si tu veux garder tes calculs pour test rapide
+        Calculator calc = new Calculator();
         System.out.println("2 + 3 = " + calc.add(2, 3));
         System.out.println("4 * 5 = " + calc.multiply(4, 5));
         System.out.println("10 / 2 = " + calc.divide(10, 2));
