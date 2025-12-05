f7f2af5b5dcfedfeec07b37d2b8f15ab8b9dcd70
khalil wendy <159011515+wendykhalil@users.noreply.github.com>
2025-12-05 19:38:48 +0100

    Update App.java

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -8,8 +8,8 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
 
-        // Si tu veux garder tes calculs pour test rapide
         Calculator calc = new Calculator();
+        System.out.println("Hello World!");
         System.out.println("2 + 3 = " + calc.add(2, 3));
         System.out.println("4 * 5 = " + calc.multiply(4, 5));
         System.out.println("10 / 2 = " + calc.divide(10, 2));
