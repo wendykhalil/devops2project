6daf540e830b00c4e986953d14cbe0c9c91aa3e5
khalilwendy <khalilwendy15@gmail.com>
2025-12-08 22:51:02 +0100

    'amv'

--- src/main/java/com/example/demo/MainApp.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-public class MainApp {
-    public static void main(String[] args) {
-        System.out.println("Application is running!");
-    }
-}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Test basique pour vérifier que le contexte Spring démarre
+    }
+}
